1152c4550b9eecab701d6168d6e5cb97e6dd0ac9
rmcodebr <rmcodebr@gmail.com>
2024-07-06 11:44:51 -0300

    Django TailwindCSS Template only with blank homepage

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./static/**/*.js",
+    // Add other paths as necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
